1bd8ebca1eeba8776b54b2b7fad0629c03577739
Nikes <107383684+Nyctophilic-nikes@users.noreply.github.com>
2025-04-05 20:12:10 +0530

    footer bottom

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { IoLogoInstagram } from 'react-icons/io'
+import { FiPhoneCall } from 'react-icons/fi'
 import { Link } from "react-router-dom"
+import { RiCopyrightLine } from 'react-icons/ri'
+
 
 const Footer = () => {
     return (
         <footer className='border-t py-12'>
-            <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0'>
+            <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4 px-4 lg:px-0'>
                 <div>
                     <h3
                         className='text-lg text-gray-800 mb-4'
@@ -17,7 +21,7 @@ const Footer = () => {
                         Be the first one to hear about the exclusive offers, new services and new accessories
                     </p>
                     <p
-                        className='font-medium text-md text-gray-400'
+                        className='font-small text-md text-gray-400'
                     >
                         Sign up and get 10% off on your first service
                     </p>
@@ -82,6 +86,34 @@ const Footer = () => {
                     </ul>
                 </div>
 
+                {/* Follow us */}
+                <div>
+                    <h3 className='text-lg text-gray-800 mb-4'>Follow Us </h3>
+                    <div
+                        className='flex items-center space-x-4 mb-6'
+                    >
+                        <a href='https://facebook.com' target='_blank'
+                            rel='nooperner noreferrer'
+                            className='hover:text-gray-500'>
+                            <IoLogoInstagram className="h-7 w-7" />
+                        </a>
+
+                    </div>
+                    <p className="text-gray-500">
+                        Call Us
+                    </p>
+                    <FiPhoneCall className='inline-block mr-2' />
+                    +91-XXXXXXXXX
+                </div>
+
+                {/* FOOTER BOTTOM */}
+
+            </div>
+            <div className='container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6'>
+                <p className='text-gray-500 text-sm tracking-tighter text-center'>
+                    <RiCopyrightLine className='inline-block mr-2' />
+                    2025, CompileTab. All Rights Reserved.
+                </p>
             </div>
         </footer>
     )
