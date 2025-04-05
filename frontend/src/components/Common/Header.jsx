4b6f892385e1c7545ab0ef357e137ba675b111ca
Nikes <107383684+Nyctophilic-nikes@users.noreply.github.com>
2025-04-05 16:14:01 +0530

    search

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+import Navbar from '../Layout/Navbar'
+
+const Header = () => {
+  return (
+      <div>
+          {/* Top Bar */}
+          <Topbar/>
+          {/* navbar */}
+          <Navbar/>
+          {/* Cart Drawer */}
+    </div>
+  )
+}
+
+export default Header
