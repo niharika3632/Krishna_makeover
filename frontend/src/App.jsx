4c8fd7c7de3f3a04af4de923a6816af61cd964d1
Nikes <107383684+Nyctophilic-nikes@users.noreply.github.com>
2025-04-07 04:51:52 +0530

    register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
 import Home from './pages/Home';
 import { Toaster } from "sonner"
-import Login from './pages/login';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           { /* User Layout*/}
           <Route index element={<Home />} />
           <Route path='Login' element={<Login />} />
+          <Route path='Register' element={<Register />} />
         </Route>
         <Route>{ /* Admin Layout*/}</Route>
       </Routes>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import login from '../assets/serviceC1.jpg'
+import serviceC2 from '../assets/serviceC2.webp'
 
-const Login = () => {
+const Register = () => {
 
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log("User login", {email, password })
+        console.log("User Registered", {name, email, password})
     }
 
     return (
         <div className={`flex`}>
-            <div
-                className={`w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12`}
-            >
+            <div className={`w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12`}>
                 <form
                     onSubmit={handleSubmit}
                     className={`w-full max-w-md bg-white p-8 rounded-lg border shadow-sm`}
@@ -29,8 +30,21 @@ const Login = () => {
                         Hey there!
                     </h2>
                     <p className={`text-center mb-6`}>
-                        Enter your username and password to login
+                        Enter your details to register
                     </p>
+
+                    {/* name */}
+                    <div className={`mb-4`}>
+                        <label className={`block text-sm font-semibold mb-2`}>
+                            Name
+                        </label>
+                        <input
+                            type='name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className={`w-full p-2 border rounded`}
+                            placeholder={`Enter your email address`} />
+                    </div>
 
                     {/* email */}
                     <div className={`mb-4`}>
@@ -54,29 +68,39 @@ const Login = () => {
                             type='password'
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            placeholder={`Enter your password`}
                             className={`w-full p-2 border rounded`}
-                        />
+                            placeholder={`Enter your password`} />
+                    </div>
+                    {/* email */}
+                    <div className={`mb-4`}>
+                        <label className={`block text-sm font-semibold mb-2`}>
+                            Confirm Password
+                        </label>
+                        <input
+                            type='password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            className={`w-full p-2 border rounded`}
+                            placeholder={`Enter your password`} />
                     </div>
 
-                    {/* submit button */}
-                    <button type='submit'
+                    <button
+                        type='submit'
                         className={`w-full bg-black text-white rounded-lg font-semibold hover:bg-gray-800 transition `}>
                         Log In
                     </button>
 
                     <p className={`mt-6 text-center text-sm`}>
-                        Don't have an account?{" "}
-                        <Link to="/register" className={`text-blue-600`}>
-                            Register</Link>
+                        Already have an account?{" "}
+                        <Link to="/Login" className={`text-blue-600`}>
+                            Log In</Link>
                     </p>
+
                 </form>
-
             </div>
-
             <div className='hidden md:block w-1/2 bg-gray-800'>
                 <div className={`h-full flex flex-col justify-center items-center`}>
-                    <img src={login} alt='Login to account'
+                    <img src={serviceC2} alt='Register to account'
                         className={`h-[750px] w-full object-cover`}
                     />
                 </div>
@@ -85,4 +109,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Register
